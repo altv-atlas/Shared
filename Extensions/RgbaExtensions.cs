c07ff6dade5dd6da4e6fcaf8f5e07cf5e028c04f
Kaiwoknats <59572199+Kaiwoknats@users.noreply.github.com>
2023-11-26 21:53:37 +0100

    -added rgb extension class from vehicles

--- Extensions/RgbaExtensions.cs
+++ Extensions/RgbaExtensions.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using AltV.Net.Data;
 
 namespace AltV.Atlas.Shared.Extensions;
@@ -15,5 +16,27 @@ public static class RgbaExtensions
     {
         var rnd = new Random( );
         return new Rgba( ( byte ) rnd.Next( 0, 255 ), ( byte ) rnd.Next( 0, 255 ), ( byte ) rnd.Next( 0, 255 ), 255 );
+    }
+    
+    /// <summary>
+    /// Tries to parse a string to a rgb color
+    /// </summary>
+    /// <param name="rgb">The rgb value</param>
+    /// <param name="color">The color value</param>
+    /// <returns>Returns if the parsing was successful</returns>
+    public static bool TryParseRgb( this string rgb, out Color color )
+    {
+        color = Color.Empty;
+
+        try
+        {
+            color = ColorTranslator.FromHtml( rgb );
+            return true;
+        }
+        catch( Exception ex )
+        {
+            Console.WriteLine( $"Error parsing color string: {ex.Message}" );
+            return false;
+        }
     }
 }
