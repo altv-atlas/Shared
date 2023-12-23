6d9fe814a5d799a4d1f0ebb5d730677ed4bc224c
Kaiwoknats <59572199+Kaiwoknats@users.noreply.github.com>
2023-12-23 20:42:31 +0100

    -added xml docs for DatabaseSettings

--- Models/DatabaseSettings.cs
+++ Models/DatabaseSettings.cs
@@ -5,7 +5,13 @@ namespace AltV.Atlas.Shared.Models;
 /// </summary>
 public class DatabaseSettings
 {
+    /// <summary>
+    /// The key by which the data can be found in appsettings.json
+    /// </summary>
     public const string Key = "Database";
 
+    /// <summary>
+    /// The connection string of your specific database
+    /// </summary>
     public string ConnectionString { get; set; }
 }
